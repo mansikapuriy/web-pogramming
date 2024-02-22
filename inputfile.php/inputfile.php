9f92d637ccf05138a2196fdd9edd65ba0e952c43
mansikapuriy <156275635+mansikapuriy@users.noreply.github.com>
2024-02-22 08:02:26 +0530

    Add files via upload

--- inputfile.php/inputfile.php
+++ inputfile.php/inputfile.php
@@ -1,1 +1,12 @@
-
+<html>
+<head>
+</head>
+<body>
+<form action="deshboard.php" method="post">
+Enter about:<input type="text" name="aboutme"/><br>
+Enter your skills:<input type="text" name="myskill"/><br>
+<!--<input type="text" name="assignment"/>-->
+<input type="submit" name="submit"/>
+</body>
+</head>
+</html>
